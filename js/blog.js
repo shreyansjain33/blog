611e0e52383a3081014f937b6a1d022f89f02132
shreyansjain33 <shreyansjain33@gmail.com>
2015-12-23 12:44:45 +0530

    edit1_6
    Connecting parse with keys

--- js/blog.js
+++ js/blog.js
@@ -1,18 +1,8 @@
-/*$(function() {
+$(function() {
  
     Parse.$ = jQuery;
- 
-    // Replace this line with the one on your Quickstart Guide Page
-	Parse.initialize("BZYDHX8VimegytEoGAITFtIgOPdZWZKQ7FCEqmi6", "pn2u753nBFtWl1jwJWCPAD5mlrGaNLwirJwoUyOV");
- 
-    var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-    testObject.save({foo: "bar"}).then(function(object) {
-      alert("You are Awesome!!");
-    });
- 
+	Parse.initialize("BZYDHX8VimegytEoGAITFtIgOPdZWZKQ7FCEqmi6", "pn2u753nBFtWl1jwJWCPAD5mlrGaNLwirJwoUyOV"); 
 });
-*/
 
 
 var Blog = Parse.Object.extend("Blog");
