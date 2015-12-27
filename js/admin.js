315e4a15bf8b3138d89e92ddfd5cabb034179601
shreyansjain33 <shreyansjain33@gmail.com>
2015-12-27 10:04:40 +0530

    edit_9

---
+++ js/admin.js
@@ -0,0 +1,50 @@
+$(function() {
+ 
+    Parse.$ = jQuery;
+	Parse.initialize("BZYDHX8VimegytEoGAITFtIgOPdZWZKQ7FCEqmi6", "pn2u753nBFtWl1jwJWCPAD5mlrGaNLwirJwoUyOV");
+
+});
+
+
+var LoginView = Parse.View.extend({
+        template: Handlebars.compile($('#login-tpl').html()),
+        events: {
+        'submit .form-signin': 'login'
+    },
+    login: function(e) {
+        // Prevent Default Submit Event
+        e.preventDefault();
+        // Get data from the form and put them into variables
+        var data = $(e.target).serializeArray(),
+            username = data[0].value,
+            password = data[1].value;
+
+        // Call Parse Login function with those variables
+        Parse.User.logIn(username, password, {
+            // If the username and password matches
+            success: function(user) {
+                var welcomeView = new WelcomeView({ model: user });
+                welcomeView.render();
+                $('.main-container').html(welcomeView.el);
+            },     
+            // If there is an error
+            error: function(user, error) {
+                console.log(error);
+            }
+        });
+    },
+        render: function(){
+        this.$el.html(this.template());
+    },
+});
+var    WelcomeView = Parse.View.extend({
+       template: Handlebars.compile($('#welcome-tpl').html()),
+        render: function(){
+            var attributes = this.model.toJSON();
+            this.$el.html(this.template(attributes));
+        }
+    });
+
+var loginView = new LoginView();
+loginView.render();
+$('.main-container').html(loginView.el);
--- js/blog.js
+++ js/blog.js
@@ -1,10 +1,8 @@
-//Parse.initialize("BZYDHX8VimegytEoGAITFtIgOPdZWZKQ7FCEqmi6", "pn2u753nBFtWl1jwJWCPAD5mlrGaNLwirJwoUyOV");
-
 $(function() {
  
     Parse.$ = jQuery;
-	Parse.initialize("BZYDHX8VimegytEoGAITFtIgOPdZWZKQ7FCEqmi6", "pn2u753nBFtWl1jwJWCPAD5mlrGaNLwirJwoUyOV");
-
+    Parse.initialize("BZYDHX8VimegytEoGAITFtIgOPdZWZKQ7FCEqmi6", "pn2u753nBFtWl1jwJWCPAD5mlrGaNLwirJwoUyOV");
+    alert("Checkpoint!");
 });
 
 
@@ -16,22 +14,22 @@ var Blogs = Parse.Collection.extend({
 
 var blogs = new Blogs();
 
+blogs.fetch({
+    success: function(blogs) {
+        alert("Awesome!");
+/*        var blogsView = new BlogsView({ collection: blogs });
+        blogsView.render();
+        $('.main-container').html(blogsView.el);
+*/    },
+    error: function(blogs, error) {
+        alert("Error!");
+    }
+});
+/*
 var BlogsView =  Parse.View.extend({
     template: Handlebars.compile($('#blogs-tpl').html()),
     render: function(){ 
         var collection = { blog: this.collection.toJSON() };
         this.$el.html(this.template(collection));
-    }
-});
-
-blogs.fetch({
-	success: function(blogs) {
-		condole.log('blogs');
-	    var blogsView = new BlogsView({ collection: blogs });
-	    blogsView.render();
-	    $('.main-container').html(blogsView.el);
-	},
-    error: function(blogs, error) {
-        console.log(error);
     }
 });
